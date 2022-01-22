347bda3c4fda941a43a9afd799073a4d02a6c445
Colin Henry <jchenry@users.noreply.github.com>
2022-01-21 20:45:28 -0800

    adding wrapper

--- render.go
+++ render.go
@@ -2,14 +2,13 @@ package pikchr
 
 import (
 	"bytes"
+	"fmt"
 
 	"github.com/gebv/pikchr"
 	"github.com/yuin/goldmark/ast"
 	"github.com/yuin/goldmark/renderer"
 	"github.com/yuin/goldmark/util"
 )
-
-// const _defaultpikchrJS = "https://cdn.jsdelivr.net/npm/pikchr/dist/pikchr.min.js"
 
 // Renderer renders pikchr diagrams as HTML, to be rendered into images
 // client side.
@@ -24,37 +23,17 @@ func (r *Renderer) RegisterFuncs(reg renderer.NodeRendererFuncRegisterer) {
 // Render renders pikchr.Block nodes.
 func (r *Renderer) Render(w util.BufWriter, src []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
 	n := node.(*Block)
-	// if entering {
-	// 	w.WriteString(`<div class="pikchr">`)
-	// 	lines := n.Lines()
-	// 	for i := 0; i < lines.Len(); i++ {
-	// 		line := lines.At(i)
-	// 		template.HTMLEscape(w, line.Value(src))
-	// 	}
-	// } else {
-	// 	w.WriteString("</div>")
-	// }
 
 	if !entering {
 		raw := r.getLines(src, n)
-		// h := sha1.New()
-		// h.Write(raw)
-		// hash := string(h.Sum([]byte{}))
-		// if result, exist := u.buf[hash]; exist {
-		// _, _ = w.Write(result)
-		// } else {
 		res, _ := pikchr.Render(
 			string(raw),
 			pikchr.HTMLError(),
 		)
 
-		svg := []byte(res.Data)
-		// if len(u.buf) >= u.MaxLength {
-		// u.buf = make(map[string][]byte)
-		// }
-		// u.buf[hash] = svg
-		_, _ = w.Write(svg)
-		// }
+		_, _ = w.WriteString(fmt.Sprintf(`<div class="pikchr-svg" style="max-width:%dx">`, res.Width))
+		_, _ = w.Write([]byte(res.Data))
+		_, _ = w.WriteString("</div>")
 	}
 
 	return ast.WalkContinue, nil
